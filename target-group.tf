71cfd8981966577bc918e376c3a3079e5f966fea
Harshitha <harshithadarshan95@gmail.com>
2023-03-02 11:19:50 +0530

    Autopush

---
+++ target-group.tf
@@ -0,0 +1,42 @@
+#  Creates the target group.
+resource "aws_lb_target_group" "app-tg" {
+  name     = "${var.COMPONENT}-${var.ENV}"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+# Attaching the instances to the created target group
+resource "aws_lb_target_group_attachment" "instance-attach" {
+  count            = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
+  target_group_arn = aws_lb_target_group.app-tg.arn
+  target_id        =  element(local.ALL_INSTANCE_IDS, count.index)
+  port             = 8080
+}
+
+
+# Backend components  target groups apart from frontend-tg has to go and be attached to Private Load Balancer Only
+# Frontend Component target groups should go and attach to Public Load Balancer Only
+
+# Adding rules to the created target group
+resource "aws_lb_listener_rule" "tg-rule" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = random_integer.priority.result
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"]
+    }
+  }
+}
+
+# Generating random interger in the range of 100 to 800
+resource "random_integer" "priority" {
+  min = 100
+  max = 800
+}
--- vars.tf
+++ vars.tf
@@ -3,5 +3,6 @@ variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
-variable "APP_PORT" {}
 variable "APP_VERSION" {}
+variable "APP_PORT" {}
+variable "ALB_TYPE" {}
